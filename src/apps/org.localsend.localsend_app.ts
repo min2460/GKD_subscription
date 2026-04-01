9b4000d35a2b4a3cde4aa15b62a999257004fe83
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-04-01 13:07:33 +0800

    perf: LocalSend 功能类-文件传输时自动点击[高级] (#93)

--- src/apps/org.localsend.localsend_app.ts
+++ src/apps/org.localsend.localsend_app.ts
@@ -7,18 +7,20 @@ export default defineGkdApp({
     {
       key: 1,
       name: '功能类-文件传输时自动点击[高级]',
+      desc: '仅触发1~2次,切换app后重置', // https://github.com/Lin-arm/GKD_subscription/discussions/93#discussioncomment-16401537
       rules: [
         {
           fastQuery: true,
+          matchRoot: true,
+          actionCd: 3500,
+          // actionMaximum: 1,
+          forcedTime: 15000,
           activityIds: '.MainActivity',
-          actionCd: 5000,
-          matches: '[desc="高级"][clickable=true]',
+          matches:
+            '@[desc="高级"] < View <2 View[childCount=2] <<4 [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/26321028',
           excludeSnapshotUrls: 'https://i.gkd.li/i/26314724', // 高级信息已显示
-          exampleUrls: [
-            'https://e.gkd.li/54a7208c-03b2-4e74-a8ed-99829d72c8c5',
-            'https://e.gkd.li/8cda8b96-aace-4137-887d-dbfe7c17b320', // 高级信息已显示
-          ],
+          exampleUrls: 'https://e.gkd.li/54a7208c-03b2-4e74-a8ed-99829d72c8c5',
         },
       ],
     },
